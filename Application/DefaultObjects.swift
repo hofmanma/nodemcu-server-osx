c5fed52a9f2ad759c0dcb8c115c356c10d179713
Matthias Hofmann <matthhofma@matthias-hofmanns-macbook-pro.local>
2018-04-10 19:02:53 +0200

    Constants

--- Application/DefaultObjects.swift
+++ Application/DefaultObjects.swift
@@ -12,8 +12,6 @@ class DefaultObjects: NSObject {
 
     static let CMD_LED_ON: String! = "LEDON"
     static let CMD_LED_OFF: String! = "LEDOFF"
-    static let CMD_REG_MCU: String! = "IAMMCU"
-    static let CMD_WHOAMI: String! = "IAMFLUFF"
     
     static let STATUS_SUCCESS: String! = "Success"
     static let STATUS_ERROR: String! = "Error"
